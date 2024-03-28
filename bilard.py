a0191463d525a4365d724061e40c0e9ad5db3222
Maciej Dems <maciej.dems@p.lodz.pl>
2024-03-28 20:42:05 +0100

    Szablon zadania

---
+++ bilard.py
@@ -0,0 +1,17 @@
+# Program symulujący „bilard na kółku". Dokładniej: wewnątrz koła jednostkowego
+# na płaszczyźnie porusza się cząstka (punkt), zachowując stałą prędkość —
+# dopóki nie napotka brzegu, czyli okręgu; wówczas odbija się zgodnie z zasadą,
+# że kąt padania równa się kątowi odbicia (chodzi o kąt pomiędzy wektorem
+# prędkości a promieniem okręgu jednostkowego w miejscu, gdzie tor ruchu
+# przecina się z okręgiem), po odbiciu jest on taki sam co do wartości ale
+# po drugiej stronie promienia. Program rysuje okręg jednostkowy oraz tor ruchu
+# cząstki wewnątrz tego okręgu, w zależności od początkowego położenia, kierunku
+# początkowej prędkości (wartość prędkości jest nieistotna, ponieważ jest stała)
+# i czasu obserwacji.
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Swój kod umieść tutaj
+
+plt.show()
